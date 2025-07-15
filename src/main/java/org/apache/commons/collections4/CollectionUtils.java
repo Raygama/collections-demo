a9851de58a65c83d49f7421c8845208e16d45e80
Raygama <daffaraygama55@gmail.com>
2025-07-15 17:22:38 +0700

    TD Test

--- src/main/java/org/apache/commons/collections4/CollectionUtils.java
+++ src/main/java/org/apache/commons/collections4/CollectionUtils.java
@@ -1438,6 +1438,7 @@ public class CollectionUtils {
      * @since 3.2
      */
     public static boolean sizeIsEmpty(final Object object) {
+        System.out.println("Simple TD Test!");
         if (object == null) {
             return true;
         } else if (object instanceof Collection<?>) {
